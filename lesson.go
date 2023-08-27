b808022c0ef7bf061e5f1c71bca36e4e99967de6
t-techa2020 <t.techa2020@gmail.com>
2023-08-27 14:43:57 +0900

    variables

--- lesson.go
+++ lesson.go
@@ -2,17 +2,29 @@ package main
 
 import "fmt"
 
-/*
-func init() {
-	fmt.Println("Init!")
-}
-*/
+// 関数外でも宣言できる
+// 型を明示的に指定したいときはvarを使う
+var (
+	i    int     = 1
+	f64  float64 = 1.2
+	s    string  = "test"
+	t, f bool    = true, false
+)
 
-func bazz() {
-	fmt.Println("Bazz")
+func foo() {
+	// ↓の方法では、関数内でしか宣言できない
+	xi := 1
+	xi = 2
+	var xf32 float32 = 1.2
+	xs := "test"
+	xt, xf := true, false
+
+	fmt.Println(xi, xf32, xs, xt, xf)
+	fmt.Printf("%T\n", xf32)
+	fmt.Printf("%T\n", xi)
 }
 
 func main() {
-	// bazz()
-	fmt.Println("Hello world!", "TEST TEST")
+	foo()
+	fmt.Println(i, f64, s, t, f)
 }
